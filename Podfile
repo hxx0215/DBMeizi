93b2d7a5a1905b7bbc20be8a3a3c0f17fd167d47
hxx0215 <huangxinxing@kdanmobile.com>
2014-08-06 21:19:05 +0800

    pod添加RAC库

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, "6.0"
+
+target "DBMeizi" do
+
+pod 'ReactiveCocoa'
+
+end
---
+++ Pods/Pods-DBMeizi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DBMeizi : NSObject
+@end
+@implementation PodsDummy_Pods_DBMeizi
+@end

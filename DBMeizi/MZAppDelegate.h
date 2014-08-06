2046932a60731da3b071621d8ee6df3958957270
hxx0215 <huangxinxing@kdanmobile.com>
2014-08-06 21:00:55 +0800

    Initial Commit

---
+++ DBMeizi/MZAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MZAppDelegate.h
+//  DBMeizi
+//
+//  Created by hxx on 8/6/14.
+//  Copyright (c) 2014 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DBMeizi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DBMeizi
+//
+//  Created by hxx on 8/6/14.
+//  Copyright (c) 2014 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MZAppDelegate class]));
+    }
+}
